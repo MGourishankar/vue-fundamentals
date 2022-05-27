a53476034339e824e0acac2a87aeebb44efcc249
KlantFirst <98264072+KlantFirst@users.noreply.github.com>
2022-05-27 23:18:52 +0200

    feat: initial commit

---
+++ vue-transactions-demo/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Header></Header>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Header from './components/Header.vue';
+
+@Options({
+  components: {
+    Header,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-transactions-demo/src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+    <div><img alt="logo" src="../assets/logo.jpg"></div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class Header extends Vue {}
+</script>
